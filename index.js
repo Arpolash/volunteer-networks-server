754a7b118e15b16dad56ac64ea432930a9d1dab6
arpolash <Arpolas2019@gmail.com>
2020-10-05 18:36:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+const port = 5000;
+
+const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://Rahman:dgdONSRizzVcNX3l@cluster0.sjfoa.mongodb.net/Volunteer?retryWrites=true&w=majority";
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PAS}@cluster0-shard-00-00.sjfoa.mongodb.net:27017,cluster0-shard-00-01.sjfoa.mongodb.net:27017,cluster0-shard-00-02.sjfoa.mongodb.net:27017/Volunteer?ssl=true&replicaSet=atlas-43pj5u-shard-0&authSource=admin&retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology : true });
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+client.connect(err => {
+  const collection = client.db(`${process.env.DB_DATABASE}`).collection(`${process.env.DB_COLLECTION}`);
+  const userCollection = client.db(`${process.env.DB_DATABASE}`).collection(`${process.env.DB_COLLECTION2}`);
+  console.log('connected')
+   app.get('/products',(req,res) =>{
+     collection.find({})
+     .toArray((err,document) =>{
+       res.send(document);
+     })
+   })
+
+   app.post("/addUser", (req, res) => {
+    const user = req.body;
+    userCollection.insertOne(user)
+    .then(result => {
+      res.redirect('http://localhost:3000/eventtasks')
+    })
+  })
+
+  app.get('/userEvents',(req,res) =>{
+    userCollection.find({email : req.query.email})
+    .toArray((err,document) =>{
+      res.send(document);
+    })
+  })
+
+  app.get('/allUserEvents',(req,res) =>{
+    userCollection.find({})
+    .toArray((err,document) =>{
+      res.send(document);
+    })
+  })
+
+
+  app.delete('/delete/:id', (req, res) =>{
+    userCollection.deleteOne({_id: ObjectId(req.params.id)})
+    .then( result => {
+      res.send(result.deletedCount > 0);
+    })
+  })
+});
+
+
+
+
+app.listen(process.env.PORT || port)
